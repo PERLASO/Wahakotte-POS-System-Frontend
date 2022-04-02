ada5af8233081253d8b6570883f69f9ee3795945
imashaaaa <imashasithmini2@gmail.com>
2022-04-02 11:16:33 +0530

    login and rest password created

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,44 +1,56 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+ import React, { useState, useEffect } from "react";
+import { useHistory, Link } from "react-router-dom";
+import { useGlobalState } from "../../GlobalState";
 import '../../style/forms.css'
 
 
-class LoginForm extends Component {
+export default function LoginForm(props) {
+    const [name, setname] = useState('');
+    const [password, setpassword] = useState('');
+    const [error, seterror] = useState('');
 
-    render() {
-        return (
-            <div className="FormPage justify-content-center">
-                <div>
-                    <div className="Form">
-                        <h3 className="text-center pb-3">User Login</h3>
-                        <form>
-                            <div className="form-group">
-                                <input type="text" className="form-control" id="username" placeholder="Enter Email here" />
-                            </div>
-                            <div className="form-group">
-                                <input type="password" className="form-control" id="username" placeholder="Enter Password here" />
-                            </div>
-                            <div className="form-group">
-                                <button className="btn btn-info w-100 mt-2">Login</button>
-                            </div>
-                            <div>
-                                <p className="text-black mt-0">Account Verified? <Link to="/password-reset-account-verify">Click to Verifiy your account</Link></p>
-                                <p className="text-black mt-0">Forgot password? <Link to="/password-reset">Click to Reset</Link></p>
-                            </div>
-                            <div className="secBreak mb-0">
-                                <hr className="FormHr" />
-                            </div>
-
-                            <div>
-                                <p className="text-black mt-0">New user?  <Link to="/registration">Click to Register</Link></p>
-                            </div>
-                        </form>
-                    </div>
-
-                </div>
-            </div>
-        )
+    let username = useGlobalState('username')[0];
+    let pass = useGlobalState('password')[0];
+  
+    const history = useHistory();
+   
+    const handleSubmit = (e) => {
+        console.log(username, pass)
+        e.preventDefault();
+        if(name ==username && password==pass){
+            history.push("/app");
+        }else{
+            seterror("User Name or Passowrd is incorrect")
+        }
+        console.log(name,password)
     }
-}
 
-export default LoginForm;
+  return (
+        <div className="FormPage justify-content-center">
+            <div>
+                <div className="Form">
+                    <h3 className="text-center pb-3">User Login</h3>
+                    <form onSubmit={(e) => {handleSubmit(e);}} >
+                        <div className="form-group">
+                            <input type="text" className="form-control" id="name" placeholder="Enter User Name here"  onChange={(e) => setname(e.target.value)} />
+                        </div>
+                        <div className="form-group">
+                            <input type="password" className="form-control" id="password" placeholder="Enter Password here" onChange={(e)=> setpassword(e.target.value)}/>
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-info w-100 mt-2">Login</button>
+                        </div>
+                        <div className=" alert-danger">{error}</div>
+                        <div className="secBreak mb-0">
+                            <hr className="FormHr" />
+                        </div>
+                        <div>
+                            <p className="text-black mt-0">Forgot password? <Link to="/password-reset">Click to Reset</Link></p>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+  )
+}
