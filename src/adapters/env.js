9d6d0bc05b3354c1caed1f293c97895391624081
vijitha mahesh <185044c@uom.lk>
2022-05-02 00:36:39 +0530

    change url

--- src/adapters/env.js
+++ src/adapters/env.js
@@ -1,6 +1,6 @@
 const env = {
     development: {
-      BASE_URL: 'https://localhost:7124/',
+    //  BASE_URL: 'https://localhost:7124/',
      //BASE_URL: 'http://localhost:8082/',
      BASE_URL: 'https://wahakotte-pos-system-backend.azurewebsites.net/',
       
