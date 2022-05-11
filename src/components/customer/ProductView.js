0f18944f4958e95a103ede43f50c0753ac9eab34
vijitha mahesh <185044c@uom.lk>
2022-05-11 12:12:47 +0530

    bug fix : ui

--- src/components/customer/ProductView.js
+++ src/components/customer/ProductView.js
@@ -133,19 +133,19 @@ class ProductView extends Component {
 
 
                     <div className="col-6 mt-4">
-                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Are you sure you want to delete this product ?</h5>
+                        <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div className="modal-dialog" role="document">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title" id="exampleModalLabel">Are you sure you want to delete this product ?</h5>
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
                                     </div>
 
                                     <div class="modal-footer d-flex">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
-                                        <button type="button" class="btn btn-primary" data-dismiss="modal" onClick={this.handledelete}>Yes</button>
+                                        <button type="button" className="btn btn-secondary" data-dismiss="modal">No</button>
+                                        <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={this.handledelete}>Yes</button>
                                     </div>
                                 </div>
                             </div>
