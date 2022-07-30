a27416284ae7a35ab98f64e54e9e8afc280e4f7e
vijitha mahesh <185044c@uom.lk>
2022-07-30 13:09:58 +0530

    bug fix

--- src/components/customer/InvoiceAdditionalRows.js
+++ src/components/customer/InvoiceAdditionalRows.js
@@ -9,21 +9,23 @@ export default class InvoiceAdditionalRows extends Component {
   }
 
   render() {
-    if(this.state.rowsCount < 27) {
-    let CounToRender = 23 - this.state.rowsCount;
-    return Array.from({ length: CounToRender}, () => (
-        <tr>
-            <td style={{height:"45px"}}></td>
+    
+  //   if(this.state.rowsCount < 27) {
+     let a = this.state.rowsCount % 26
+     let CounToRender = 23 - (this.state.rowsCount % 26) ;
+     return Array.from({ length: CounToRender}, () => (
+         <tr>
+             <td style={{height:"45px"}}></td>
         </tr>
-      ));
-    }
-    else {
-        let CounToRender = (33 - ((this.state.rowsCount-26) % 33 ))- (Math.floor(this.state.rowsCount/33));
-    return Array.from({ length: CounToRender}, () => (
-        <tr>
-            <td style={{height:"45px"}}></td>
-        </tr>
-      ));
-    }
+     ));
   }
+  //   else {
+  //       let CounToRender = (33 - ((this.state.rowsCount-26) % 33 ))- (Math.floor(this.state.rowsCount/33));
+  //   return Array.from({ length: CounToRender}, () => (
+  //       <tr>
+  //           <td style={{height:"45px"}}></td>
+  //       </tr>
+  //     ));
+  //   }
+  // }
 }
